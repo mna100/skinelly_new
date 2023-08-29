76ee714f7d630aab6181be4825ce15eb2de763a9
Nikita <nr@mna100.ru>
2023-08-29 13:19:41 +0300

    BT#19456 fix pagination

--- wp-content/themes/skinelly/archive-events.php
+++ wp-content/themes/skinelly/archive-events.php
@@ -1,21 +1,22 @@
 <?php
-	get_header();
-	$id = get_post_type() . '_options';
+get_header();
+$id = get_post_type() . '_options';
 ?>
 
 
 <div class="events">
-    <div class="container">
-        <div class="events-header">
-            <div class="title-section">
-                <h1>
+	<div class="container">
+		<div class="events-header">
+			<div class="title-section">
+				<h1>
 					<?= (get_meta($id)["h1"]) ? get_meta($id)["h1"] : the_archive_title(); ?>
-                </h1>
-            </div>
-        </div>
+				</h1>
+			</div>
+		</div>
 
 
-		<? //Ближайшие мероприятия?>
+		<? //Ближайшие мероприятия
+		?>
 		<? $date_now = date('Y-m-d H:i:s'); ?>
 
 		<? $args = [
@@ -34,117 +35,118 @@
 			],
 		];
 
-			$wp_query = new WP_Query($args);
+		$wp_query = new WP_Query($args);
 		?>
 
-		<? if ($wp_query->have_posts()): ?>
+		<? if ($wp_query->have_posts()) : ?>
 
-            <h2 class="events-title-section">Ближайшие мероприятия</h2>
+			<h2 class="events-title-section">Ближайшие мероприятия</h2>
 
-            <div class="events-list">
+			<div class="events-list">
 				<? while ($wp_query->have_posts()) :
 					$wp_query->the_post(); ?>
-                    <div class="events-item">
+					<div class="events-item">
 
-                        <a href="<?php the_permalink(); ?>" class="events-image">
+						<a href="<?php the_permalink(); ?>" class="events-image">
 							<? $img = get_the_post_thumbnail($post->ID, 'full'); ?>
-							<? if ($img): ?>
+							<? if ($img) : ?>
 								<? echo $img; ?>
-							<? else: ?>
-                                <img src="<?= get_template_directory_uri() . '/public/no-photo.png' ?>" alt="<? the_title(); ?>">
+							<? else : ?>
+								<img src="<?= get_template_directory_uri() . '/public/no-photo.png' ?>" alt="<? the_title(); ?>">
 							<? endif; ?>
-                        </a>
-                        <div class="events-wrap-title">
-                            <div class="events-date"><?= substr(get_field("date", $post->id), 0, -5); ?>
-								<? if (get_field("date_end", $post->id)): ?>
-                                    -<?= substr(get_field("date_end", $post->id), 0, -5); ?>
+						</a>
+						<div class="events-wrap-title">
+							<div class="events-date"><?= substr(get_field("date", $post->id), 0, -5); ?>
+								<? if (get_field("date_end", $post->id)) : ?>
+									-<?= substr(get_field("date_end", $post->id), 0, -5); ?>
 								<? endif; ?>
-                            </div>
-                            <div class="events-title">
+							</div>
+							<div class="events-title">
 								<?= (get_field('theme_title', $post->id)) ? get_field('theme_title', $post->id) : the_title(); ?>
-                            </div>
-							<? if (get_field("city", $post->id)): ?>
-                                <div class="events-city"><?= get_field("city", $post->id); ?></div>
+							</div>
+							<? if (get_field("city", $post->id)) : ?>
+								<div class="events-city"><?= get_field("city", $post->id); ?></div>
 							<? endif; ?>
-                        </div>
-						<? if (get_field("theme", $post->id)): ?>
-                            <div class="events-text"><?= get_field("theme", $post->id); ?></div>
+						</div>
+						<? if (get_field("theme", $post->id)) : ?>
+							<div class="events-text"><?= get_field("theme", $post->id); ?></div>
 						<? endif; ?>
-                        <a href="<?php the_permalink(); ?>" class="button button_tr events-btn">Подробнее</a>
+						<a href="<?php the_permalink(); ?>" class="button button_tr events-btn">Подробнее</a>
 
-                    </div>
+					</div>
 				<? endwhile; ?>
 
-				<? wp_reset_query(); ?>
-            </div>
+			</div>
 		<? endif; ?>
+		<? wp_reset_query(); ?>
 
-
-		<? //Прошедшие мероприятия?>
+		<? //Прошедшие мероприятия
+		?>
 		<?
-			$args = [
-				'posts_per_page' => 6,
-				'post_type'      => 'events',
-				'orderby'        => 'date',
-				'order'          => 'desc',
-				'meta_query'     => [
-					[
-						'key'     => 'date',
-						'compare' => '<=',
-						'value'   => $date_now,
-						'type'    => 'DATETIME',
-					],
-
+		$args = [
+			'posts_per_page' => 3,
+			'post_type'      => 'events',
+			'orderby'        => 'date',
+			'order'          => 'desc',
+			'meta_query'     => [
+				[
+					'key'     => 'date',
+					'compare' => '<=',
+					'value'   => $date_now,
+					'type'    => 'DATETIME',
 				],
-				'paged'          => get_query_var('paged') ? get_query_var('paged') : 1,
-			];
 
-			$wp_query_past = new WP_Query($args);
+			],
+			'paged'          => get_query_var('paged') ? get_query_var('paged') : 1,
+		];
+
+		$wp_query_past = new WP_Query($args);
 		?>
 
-		<? if ($wp_query_past->have_posts()): ?>
+		<? if ($wp_query_past->have_posts()) : ?>
 
-            <h2 class="events-title-section">Прошедшие мероприятия</h2>
+			<h2 class="events-title-section">Прошедшие мероприятия</h2>
 
-            <div class="events-list">
+			<div class="events-list">
 				<? while ($wp_query_past->have_posts()) :
 					$wp_query_past->the_post(); ?>
 
-                    <div class="events-item">
+					<div class="events-item">
 
-                        <a href="<?php the_permalink(); ?>" class="events-image">
+						<a href="<?php the_permalink(); ?>" class="events-image">
 							<? $img = get_the_post_thumbnail($post->ID, 'full'); ?>
-							<? if ($img): ?>
+							<? if ($img) : ?>
 								<? echo $img; ?>
-							<? else: ?>
-                                <img src="<?= get_template_directory_uri() . '/public/no-photo.png' ?>" alt="<? the_title(); ?>">
+							<? else : ?>
+								<img src="<?= get_template_directory_uri() . '/public/no-photo.png' ?>" alt="<? the_title(); ?>">
 							<? endif; ?>
-                        </a>
-                        <div class="events-wrap-title">
-                            <div class="events-date"><?= substr(get_field("date", $post->id), 0, -5); ?>
-								<? if (get_field("date_end", $post->id)): ?>
-                                    -<?= substr(get_field("date_end", $post->id), 0, -5); ?>
+						</a>
+						<div class="events-wrap-title">
+							<div class="events-date"><?= substr(get_field("date", $post->id), 0, -5); ?>
+								<? if (get_field("date_end", $post->id)) : ?>
+									-<?= substr(get_field("date_end", $post->id), 0, -5); ?>
 								<? endif; ?>
-                            </div>
-                            <div class="events-title">
+							</div>
+							<div class="events-title">
 								<?= (get_field('theme_title', $post->id)) ? get_field('theme_title', $post->id) : the_title(); ?>
-                            </div>
-							<? if (get_field("city", $post->id)): ?>
-                                <div class="events-city"><?= get_field("city", $post->id); ?></div>
+							</div>
+							<? if (get_field("city", $post->id)) : ?>
+								<div class="events-city"><?= get_field("city", $post->id); ?></div>
 							<? endif; ?>
-                        </div>
-						<? if (get_field("theme", $post->id)): ?>
-                            <div class="events-text"><?= get_field("theme", $post->id); ?></div>
+						</div>
+						<? if (get_field("theme", $post->id)) : ?>
+							<div class="events-text"><?= get_field("theme", $post->id); ?></div>
 						<? endif; ?>
-                        <a href="<?php the_permalink(); ?>" class="button button_tr events-btn">Подробнее</a>
+						<a href="<?php the_permalink(); ?>" class="button button_tr events-btn">Подробнее</a>
 
-                    </div>
+					</div>
 
 				<? endwhile; ?>
-            </div>
+			</div>
 
-			<? // Pagination?>
-			<? if ($wp_query_past->max_num_pages > 1): ?>
+			<? // Pagination
+			?>
+			<? if ($wp_query_past->max_num_pages > 1) : ?>
 				<?
 				$big   = 999999999; // need an unlikely integer
 				$links = paginate_links([
@@ -158,18 +160,18 @@
 
 				]);
 				?>
-				<? if (!empty($links)): ?>
-                    <ul class="pagination">
-						<? foreach ($links as $link): ?>
-                            <li><?php echo $link; ?></li>
+				<? if (!empty($links)) : ?>
+					<ul class="pagination">
+						<? foreach ($links as $link) : ?>
+							<li><?php echo $link; ?></li>
 						<? endforeach; ?>
-                    </ul>
+					</ul>
 				<? endif; ?>
 			<? endif; ?>
 			<? wp_reset_query(); ?>
 		<? endif; ?>
 
-    </div>
+	</div>
 
 
 </div>
